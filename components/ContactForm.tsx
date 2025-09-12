c47bac714022b2c71eced8a4955b0cd543ff0feb
sjiya114 <sjiya04072004@gmail.com>
2025-09-13 03:51:06 +0530

    edit

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -110,7 +110,7 @@ export default function ContactForm() {
             type="submit"
             className="block w-full rounded-md bg-indigo-500 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
           >
-            Let's talk
+            Lets talk
           </button>
         </div>
       </form>

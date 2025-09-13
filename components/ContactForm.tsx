278f59842a809f19559aa6c9106fbadc7e86385e
sjiya114 <sjiya04072004@gmail.com>
2025-09-14 01:40:04 +0530

    edit

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,6 +1,54 @@
+"use client";
 import { ChevronDownIcon,PhoneCall,Mail } from 'lucide-react';
+import axios from 'axios';
+import { useState } from 'react';
+import React from 'react';
+
 
 export default function ContactForm() {
+  const [data,setData]=useState({
+   FirstName:"",
+   LastName:"",
+   Message:"",
+   PhoneNumber:"",
+   Email:""
+  })
+  const onChangeHandler=(e)=>
+  {
+    setData({...data,[e.target.name]:e.target.value});
+  }
+  const handleSubmit=async(e)=>
+  {
+     e.preventDefault();
+     let formData=new FormData();
+     formData.append("FirstName",data.FirstName);
+     formData.append("LastName",data.LastName);
+     formData.append("Message",data.Message);
+     formData.append("Email",data.Email);
+     formData.append("PhoneNumber",data.PhoneNumber);
+     formData.append("access_key","49baf150-b86e-46f4-8511-38af61de8664");
+     try {
+      const res=await axios.post("https://api.web3forms.com/submit",formData, {headers: {
+                "Content-Type":"application/json"
+            }});
+      if(res.data.success)
+      {
+         console.log("form submitted successfully");
+         setData({
+          Email:"",
+          FirstName:"",
+          LastName:"",
+          Message:"",
+          PhoneNumber:""
+         })
+      }
+      else{
+        console.log("error");
+      }
+     } catch (error) {
+      console.log("error");
+     }
+  }
   return (
     <div id='contact' className='flex flex-row bg-gradient-to-r  from-gray-950 to-gray-900 max-md:flex-col'>
      <div className='w-1/2 flex flex-col max-md:w-full space-y-6 justify-center  px-6 py-24 sm:py-32 lg:pl-40 text-white'>
@@ -29,16 +77,18 @@ export default function ContactForm() {
           className="relative left-1/2 -z-10 aspect-1155/678 w-144.5 max-w-none -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%-40rem)] sm:w-288.75"
         />
       </div>
-      <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
+      <form onSubmit={handleSubmit} encType='form-data' method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
+        
         <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
           <div>
-            <label htmlFor="first-name" className="block text-sm/6 font-semibold text-white">
+            <label htmlFor="first-name"  className="block text-sm/6 font-semibold text-white">
               First name
             </label>
             <div className="mt-2.5">
               <input
                 id="first-name"
-                name="first-name"
+                name="FirstName"
+                onChange={(e)=>{onChangeHandler(e)}}
                 type="text"
                 autoComplete="given-name"
                 className="block w-full rounded-md bg-white/5 px-3.5 py-2 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500"
@@ -52,7 +102,8 @@ export default function ContactForm() {
             <div className="mt-2.5">
               <input
                 id="last-name"
-                name="last-name"
+                name="LastName"
+                onChange={(e)=>{onChangeHandler(e)}}
                 type="text"
                 autoComplete="family-name"
                 className="block w-full rounded-md bg-white/5 px-3.5 py-2 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500"
@@ -66,7 +117,8 @@ export default function ContactForm() {
             <div className="mt-2.5">
               <input
                 id="email"
-                name="email"
+                name="Email"
+                onChange={(e)=>{onChangeHandler(e)}}
                 type="email"
                 autoComplete="email"
                 className="block w-full rounded-md bg-white/5 px-3.5 py-2 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500"
@@ -81,8 +133,9 @@ export default function ContactForm() {
               <div className="flex rounded-md bg-white/5 outline-1 -outline-offset-1 outline-white/10 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-500">
                 <input
                   id="phone-number"
-                  name="phone-number"
+                  name="PhoneNumber"
                   type="text"
+                  onChange={(e)=>{onChangeHandler(e)}}
                   placeholder="123-456-7890"
                   className="block min-w-0 grow bg-transparent py-1.5 pr-3 pl-1 text-base text-white placeholder:text-gray-500 focus:outline-none sm:text-sm/6"
                 />
@@ -96,7 +149,8 @@ export default function ContactForm() {
             <div className="mt-2.5">
               <textarea
                 id="message"
-                name="message"
+                name="Message"
+                onChange={(e)=>{onChangeHandler(e)}}
                 rows={4}
                 className="block w-full rounded-md bg-white/5 px-3.5 py-2 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500"
                 defaultValue={''}
